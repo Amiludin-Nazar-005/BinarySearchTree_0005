5c7fafe8396d4dcc28a8c5ed23f00ba30d46eada
Amiludin-Nazar-005 <amiludin.nazar.ft24@mail.umy.ac.id>
2025-06-26 11:16:32 +0700

    membuat kelas node untuk public

---
+++ binarytreee.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class node {
+    public:
+    int info;
+    node *leftchild;
+    node *rightchild;
+
+    node ()
+    {
+        leftchild = nullptr;
+        rightchild =nullptr;
+    }
+};
